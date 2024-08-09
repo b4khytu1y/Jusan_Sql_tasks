4a52cbcff6006a65998840d8774f782ac3a8fa38
B4khytu1y <bakhytulynurzhan@gmail.com>
2024-08-09 23:19:19 +0500

    Create sql files

---
+++ tasks/CreateSQLFiles.java
@@ -0,0 +1,24 @@
+package java_files;
+import java.io.File;
+import java.io.IOException;
+
+public class CreateSQLFiles {
+    public static void main(String[] args) {
+        for (int i = 1; i <= 24; i++) {
+            String fileName = "task_" + i + ".sql";
+            File file = new File(fileName);
+
+            try {
+                if (file.createNewFile()) {
+                    System.out.println("File created: " + fileName);
+                } else {
+                    System.out.println("File already exists: " + fileName);
+                }
+            } catch (IOException e) {
+                System.out.println("An error occurred while creating the file: " + fileName);
+                e.printStackTrace();
+            }
+        }
+    }
+}
+
